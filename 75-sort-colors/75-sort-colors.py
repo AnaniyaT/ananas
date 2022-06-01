4e508ecff55b5d76b8c86d2830c05ac84eb69539
AnaniyaT <49951520+AnaniyaT@users.noreply.github.com>
2022-06-01 23:23:44 +0300

    Time: 57 ms (26.14%), Space: 14 MB (14.93%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,10 +1,19 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        arr = [0,0,0]
+        n0 = 0
+        n1 = 0
+        n2 = 0
         for i in nums:
-            arr[i]+=1
-        ind = 0
-        for l in range (0,3):
-            for j in range (0,arr[l]):
-                nums[ind] = l
-                ind+=1
+            if i == 0: n0 += 1
+            elif i == 1: n1 += 1
+            elif i == 2: n2 += 1
+        k = 0
+        for i in range (0,n0):
+            nums[k] = 0
+            k+=1
+        for i in range (0,n1):
+            nums[k] = 1
+            k+=1
+        for i in range (0,n2):
+            nums[k] = 2
+            k+=1
