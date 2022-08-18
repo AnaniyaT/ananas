97dbaf182d76d64440e39837420bf7f9fc04feb6
AnaniyaT <49951520+AnaniyaT@users.noreply.github.com>
2022-08-18 17:17:46 +0300

    Time: 635 ms (91.36%), Space: 31.4 MB (91.49%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,20 +1,14 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        counter = {}
+        freq = Counter(arr)
+        freqlist = sorted(freq, key = freq.get, reverse = True)
+        acc = 0
+        half = len(arr)//2
         n = 0
-        sum = 0
-        for i in arr:
-            if i in counter:
-                counter[i] +=1
-            else:
-                counter[i] = 1
-        counterlist = sorted(counter, key = counter.get)
-        for i in reversed(counterlist):
-            if sum  < len(arr)//2:
-                sum += counter[i]
+        for i in freqlist:
+            if acc <  half:
+                acc += freq[i]
                 n += 1
             else:
-                break
-        return n
-            
-        
+                return n
+        return 1
