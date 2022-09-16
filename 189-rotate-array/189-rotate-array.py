5bd94713bd0af74d3827fd466a7210d54d04058a
Anan <49951520+AnaniyaT@users.noreply.github.com>
2022-09-16 17:16:44 +0300

    Time: 568 ms (27.25%), Space: 25.4 MB (29.25%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,6 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        temp = list(nums)
-        for i,j in enumerate(temp):
-            nums[(i+k)%len(nums)] = j
+        k%=len(nums)
+        for i in range(len(nums)//2):
+            nums[i], nums[len(nums)-i-1] = nums[len(nums)-i-1], nums[i]
+        for i in range(k//2):
+            nums[i], nums[k-i-1] = nums[k-i-1], nums[i]
+        for i in range((len(nums)-k)//2):
+            nums[i+k], nums[len(nums)-i-1] = nums[len(nums)-i-1], nums[i+k]
+            
