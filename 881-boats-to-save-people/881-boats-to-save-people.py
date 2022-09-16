d577049e2f2fe2694b10dc6f5f6f763cad72ea8f
Anan <49951520+AnaniyaT@users.noreply.github.com>
2022-09-16 16:13:27 +0300

    Time: 1069 ms (13.97%), Space: 20.9 MB (30.20%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -4,12 +4,9 @@ class Solution:
         boats, l, r = 0, 0, len(people) -1
         while l <= r:
             if not people[l]+people[r] > limit:
-                boats += 1
                 l += 1 
-                r -= 1
-            else:
-                boats += 1
-                r -= 1
+            boats += 1
+            r -= 1
         return boats
 
             
