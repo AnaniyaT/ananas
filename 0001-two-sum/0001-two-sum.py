2cf4b9486ac9b90b5baf2473ca67640bcff3b772
Anan <49951520+AnaniyaT@users.noreply.github.com>
2022-11-10 10:02:48 +0300

    Time: 59 ms (97.67%), Space: 15 MB (69.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indxdic = {}
         for i, j in enumerate(nums):
-            nums[i] = (j, i)
-        nums = sorted(nums)
-        l, r = 0, len(nums) - 1 
-        while l < r:
-            if nums[l][0] + nums[r][0] < target:
-                l += 1
-            elif nums[l][0] + nums[r][0] > target:
-                r -= 1
+            if target-j in indxdic:
+                return [i, indxdic[target-j]]
             else:
-                return [nums[l][1], nums[r][1]]
+                indxdic[j] = i
