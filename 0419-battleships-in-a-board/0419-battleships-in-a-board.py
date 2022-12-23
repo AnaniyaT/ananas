000f5024bb257ae453e386b0ffdfd312a0290419
Anan <49951520+AnaniyaT@users.noreply.github.com>
2022-12-23 19:25:32 +0300

    Time: 77 ms (89.63%), Space: 14.7 MB (22.94%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        counter = 0
+        for rowNo, row in enumerate(board):
+            for col, cell in enumerate(row):
+                leftCell = "."
+                topCell = "."
+                if rowNo > 0:
+                    topCell = board[rowNo-1][col]
+                if col > 0:
+                    leftCell = board[rowNo][col-1]
+                if cell == "X":
+                    if leftCell != ".":
+                        board[rowNo][col] = leftCell
+                    elif topCell != ".":
+                        board[rowNo][col] = topCell
+                    else:
+                        counter += 1
+                        board[rowNo][col] = counter
+        print (board)
+        return counter
