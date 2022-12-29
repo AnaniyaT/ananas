17b499a01e30c2a124d5e3c5019b2d788753386b
Anan <49951520+AnaniyaT@users.noreply.github.com>
2022-12-29 19:01:00 +0300

    Time: 1197 ms (5.54%), Space: 13.9 MB (93.85%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -9,7 +9,7 @@ class Solution:
             for right in range(len(s)):
                 if s[right] != target:
                     ops -= 1
-                while ops < 0 and s[right] != target:
+                while ops < 0:
                     if s[left] != target:
                         ops += 1
                     left += 1
