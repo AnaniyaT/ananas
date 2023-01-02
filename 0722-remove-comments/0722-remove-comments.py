5aeded8b786e5eff202f753f8040d5d5015465ab
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-01-02 19:28:31 +0300

    Time: 32 ms (87.51%), Space: 13.9 MB (43.31%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.py
+++ 0722-remove-comments/0722-remove-comments.py
@@ -4,27 +4,21 @@ class Solution:
         commentFlag = False
         tempLine = []
         
-        
         for line in source:
             ignoreInd = -1
             length = len(line)
             for ind, char in enumerate(line):
                 if ind == ignoreInd:
                     continue
+                    
                 if not commentFlag:
-                    if char == "/" and ind < length - 1:
-                        if line[ind + 1] == "/":
-                            if tempLine:
-                                lineString = "".join(tempLine)
-                                resultArr.append(lineString)
-                                tempLine = []  
-                            break
-                            
-                        elif line[ind + 1] == "*":
+                    if char == "/" and ind < length - 1 and line[ind + 1] == "/":  
+                        break
+                        
+                    elif char == "/" and ind < length - 1 and  line[ind + 1] == "*":
                             ignoreInd = ind + 1
                             commentFlag = True
-                        else:
-                            tempLine.append(char)
+                            
                     else:
                         tempLine.append(char)
                         
