affafc1767503b3108b0aae0744688cc65bb47e8
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-01-17 18:51:20 +0300

    Time: 514 ms (11.88%), Space: 14.4 MB (42.89%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,33 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(len(names)):
-            swaps = 0
-            for ind in range(len(names)-i-1):
-                if heights[ind] < heights[ind+1]:
-                    heights[ind], heights[ind+1] = heights[ind+1], heights[ind]
-                    names[ind], names[ind+1] = names[ind+1], names[ind]
-                    swaps += 1
+        # Bubble sort
+#         for i in range(len(names)):
+#             swaps = 0
+#             for ind in range(len(names)-i-1):
+#                 if heights[ind] < heights[ind+1]:
+#                     heights[ind], heights[ind+1] = heights[ind+1], heights[ind]
+#                     names[ind], names[ind+1] = names[ind+1], names[ind]
+#                     swaps += 1
                     
-            if not swaps:
-                break
+#             if not swaps:
+#                 break
                 
+#         return names
+        #selection sort
+        
+        length = len(names)
+        
+        for i in range(length):
+            maxInd = i
+            maxx = 0
+            for ind in range(i, length):
+                if heights[ind] > maxx:
+                    maxx = heights[ind]
+                    maxInd = ind
+            
+            heights[i], heights[maxInd] = heights[maxInd], heights[i]
+            names[i], names[maxInd] = names[maxInd], names[i]
+            
         return names
+            
+                
