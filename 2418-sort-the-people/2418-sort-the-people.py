5fbfc9857229f0dcafe8b4169ea8b32a08ee3dcf
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-01-17 20:03:02 +0300

    Time: 1009 ms (7.74%), Space: 21.6 MB (8.95%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -13,21 +13,45 @@ class Solution:
 #                 break
                 
 #         return names
+
         #selection sort
         
-        length = len(names)
+#         length = len(names)
         
-        for i in range(length):
-            maxInd = i
-            maxx = 0
-            for ind in range(i, length):
-                if heights[ind] > maxx:
-                    maxx = heights[ind]
-                    maxInd = ind
+#         for i in range(length):
+#             maxInd = i
+#             maxx = 0
+#             for ind in range(i, length):
+#                 if heights[ind] > maxx:
+#                     maxx = heights[ind]
+#                     maxInd = ind
             
-            heights[i], heights[maxInd] = heights[maxInd], heights[i]
-            names[i], names[maxInd] = names[maxInd], names[i]
+#             heights[i], heights[maxInd] = heights[maxInd], heights[i]
+#             names[i], names[maxInd] = names[maxInd], names[i]
             
-        return names
-            
+#         return names
+
+        # insertion sort
+        
+#         for i in range(1, len(names)):
+#             for ind in range(i-1, -1, -1):
+#                 if heights[ind] < heights[i]:
+#                     heights[ind], heights[ind+1] = heights[ind+1], heights[ind]
+#                     names[ind], names[ind+1] = names[ind+1], names[ind]
+#                 else:
+#                     break
+                    
+#         return names
+
+        # counting sort
+    
+        countArr = [[] for _ in range(max(heights)+1)]
+        for ind in range(len(heights)-1, -1, -1):
+            countArr[heights[ind]].append(names[ind])
+        
+        result = []
+        for arr in reversed(countArr):
+            result.extend(arr)
+        
+        return result
                 
