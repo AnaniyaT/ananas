af6f9a399e37c156ffb79fda0c993f42b86ec7fb
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-01-26 18:17:35 +0300

    Time: 51 ms (71.48%), Space: 13.8 MB (59.56%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -1,18 +1,15 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        n = max(nums)
-        freqarr = [0]*(n+1)
-        for i in nums:
-            freqarr[i] += 1
-        sortedarr = []
-        k=0
-        for j in freqarr:
-            sortedarr += [k]*j
-            k+=1
-        result = []
-        for l in range (0,len(sortedarr)):
-            if sortedarr[l] == target:
-                result.append(l)
-        return(result)
+        numCount = 0
+        greaterThan = 0
+        for num in nums:
+            if num < target:
+                greaterThan += 1
+            elif num == target:
+                numCount += 1
+                
+        return [greaterThan+n for n in range(numCount)]
+        
+                
             
    
