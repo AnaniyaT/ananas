c2eab5d32d565524e6a0dfba1d6aeba7ef8927ed
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-02-07 11:53:44 +0300

    Time: 95 ms (93.41%), Space: 18.3 MB (33.50%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,23 +5,42 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        newList = ListNode(0)
-        newCurr = newList
+#         newList = ListNode(0)
+#         newCurr = newList
         
-        isNotEmpty = True
+#         isNotEmpty = True
         
-        while isNotEmpty:
-            minn = [float('inf'), -1]
+#         while isNotEmpty:
+#             minn = [float('inf'), -1]
             
-            for ind, lst in enumerate(lists):
-                if lst and lst.val < minn[0]:
-                    minn = [lst.val, ind]
+#             for ind, lst in enumerate(lists):
+#                 if lst and lst.val < minn[0]:
+#                     minn = [lst.val, ind]
             
-            if minn[0] != float('inf'):
-                newCurr.next = ListNode(minn[0])
-                newCurr = newCurr.next
-                lists[minn[1]] = lists[minn[1]].next
-            else:
-                isNotEmpty = False
+#             if minn[0] != float('inf'):
+#                 newCurr.next = ListNode(minn[0])
+#                 newCurr = newCurr.next
+#                 lists[minn[1]] = lists[minn[1]].next
+#             else:
+#                 isNotEmpty = False
                 
+#         return newList.next
+
+        heap = []
+        heapq.heapify(heap)
+        
+        for lst in lists:
+            while lst:
+                heapq.heappush(heap, lst.val)
+                lst = lst.next
+                
+        newList = ListNode(0)
+        curr = newList
+        
+        while heap:
+            val = heappop(heap)
+            curr.next = ListNode(val)
+            curr = curr.next
+            
         return newList.next
+        
