944f19c739fd8f55cbd35c3319cb9d60e28aec3f
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-02-25 22:01:14 +0300

    Time: 66 ms (97.05%), Space: 14.4 MB (80.68%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,12 +1,15 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        listt = [0]*1001
-        summ = 0
-        for i,j,k in trips:
-            listt[j] += i
-            listt[k] -= i
-        for i in listt:
-            summ += i
-            if summ > capacity:
+        locations = [0]*1001
+        
+        for num, pick, drop in trips:
+            locations[pick] += num
+            locations[drop] -= num
+            
+        runSum = 0
+        for passengers in locations:
+            runSum += passengers
+            if runSum > capacity:
                 return False
+            
         return True
