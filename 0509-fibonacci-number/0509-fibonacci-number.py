be70bb3abe4bca6d8b76850f35cec62cd68ef664
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-02-28 11:04:36 +0300

    Time: 33 ms (67.91%), Space: 13.8 MB (44.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,14 @@
 class Solution:
-    @cache
+    def __init__(self):
+        self.fibList = [0, 1]
+        
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        return self.fib(n-1)+self.fib(n-2)
+        if n < len(self.fibList):
+            return self.fibList[n]
         
+        before = self.fib(n-2)
+        after = self.fib(n-1)
         
+        self.fibList.append(before + after)
+        
+        return self.fibList[n]
