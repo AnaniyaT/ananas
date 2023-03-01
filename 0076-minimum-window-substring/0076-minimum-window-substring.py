bdd08f96a3e9b6bad8f500203d56e6a30a52511d
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-03-01 10:14:27 +0300

    Time: 804 ms (9.32%), Space: 14.6 MB (99.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,18 +3,20 @@ class Solution:
         l = 0
         count = Counter(t)
         current = {lett:0 for lett in t}
-        ans = ""
+        ans = [-float('inf'), float('inf')]
         
         for r in range(len(s)):
             lett = s[r]
             if lett in current:
                 current[lett] += 1
-            while all([current[letter] >= count[letter] for letter in current]):
+            while all(current[letter] >= count[letter] for letter in current):
                 leftLett = s[l]
                 if leftLett in current:
                     current[leftLett] -= 1
-                if r-l < len(ans) or not ans:
-                    ans = s[l:r+1]
+                ans = min([ans, [l, r]], key=lambda x:x[1]-x[0])
                 l += 1
                     
-        return ans
+        if ans[0] == -float('inf'):
+            return ""
+        
+        return s[ans[0]:ans[1]+1]
