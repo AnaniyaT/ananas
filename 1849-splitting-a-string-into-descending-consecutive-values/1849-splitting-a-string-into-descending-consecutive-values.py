7cded4af5c281a41835bf3bec17875fb71688d32
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-03-09 15:49:49 +0300

    Time: 37 ms (61.97%), Space: 13.8 MB (63.61%) - LeetHub

---
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -0,0 +1,50 @@
+class Solution:
+    def splitString(self, s: str) -> bool:
+        slices = []
+        found = False
+        
+        def check():
+            if len(slices) < 2:
+                return False
+            
+            for ind in range(1, len(slices)):
+                if int(slices[ind - 1]) - int(slices[ind]) != 1:
+                    return False
+                
+            return True
+        
+        def backtrack(strg):
+            nonlocal found
+            
+            if found:
+                return
+            
+            if not strg:
+                if check():
+                    found = True
+                return
+            
+            n = len(strg)
+            
+            for i in range(1, n+1):
+            
+                slices.append(strg[:i])
+                
+                if len(slices) > 1:
+                    first = int(slices[-2])
+                    second = int(slices[-1])
+                    
+                    if first - second != 1:
+                        slices.pop()
+                        continue
+                
+                backtrack(strg[i:])
+    
+                slices.pop()
+                
+                
+        backtrack(s)
+        
+        return found
+                
+            
