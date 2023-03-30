f397790424c25ddc43caa4a545c78b9be87370e9
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-03-30 18:29:42 +0300

    Update 0268-missing-number.py

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,15 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        numbers = set(range(len(nums)+1))
-        for num in nums:
-            numbers.remove(num)
-        return numbers.pop()
+        missNum = -1
+        
+        for ind in range(len(nums)):
+            
+            while nums[ind] != ind + 1:
+                num = nums[ind]
+                if not num:
+                    missNum = ind
+                    break
+                    
+                nums[ind], nums[num - 1] = nums[num - 1], nums[ind]
+                
+        return missNum + 1
