408fab144560a0f13518d73956b35e328d002d83
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-05-18 18:37:00 +0300

    Time: 2031 ms (52.49%), Space: 107.3 MB (59.09%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,40 +1,44 @@
-class Solution:
-    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        rep = [i for i in range(n)]
-        rank = [1 for i in range(n)]
-        
-        def findRep(x):
-            cur = x
-            while cur != rep[cur]:
-                cur = rep[cur]
-                
-            while x != cur:
-                prev = rep[x]
-                rep[x] = cur
-                x = prev
-                
-            return cur
-                
-        def union(x, y):
-            repX, repY = findRep(x), findRep(y)
-            
-            if rank[repX] < rank[repY]:
-                rep[repX] = repY
-                rank[repY] += rank[repX]
-            else:
-                rep[repX] = repX
-                rep[repY] = repX
-                rank[repX] += rank[repY]
-                
-        def isConnected(x, y):
-            return findRep(x) == findRep(y)
+
+class UnionFind:
+    def __init__(self, n, edges=[]) -> None:
+        self.rep = [i for i in range(n)]
+        self.rank = [1 for i in range(n)]
         
         for fro, to in edges:
-            union(fro, to)
+            self.union(fro, to)
+    
+    def findRep(self, x):
+        cur = x
+        while cur != self.rep[cur]:
+            cur = self.rep[cur]
             
-        # print(rep)
-        # print(rank)
-        return isConnected(source, destination)
+        while x != cur:
+            prev = self.rep[x]
+            self.rep[x] = cur
+            x = prev
+            
+        return cur
+        
+    def union(self, x, y):
+        repX, repY = self.findRep(x), self.findRep(y)
+        
+        if self.rank[repX] < self.rank[repY]:
+            self.rep[repX] = repY
+            self.rank[repY] += self.rank[repX]
+        else:
+            self.rep[repX] = repX
+            self.rep[repY] = repX
+            self.rank[repX] += self.rank[repY]
+            
+    def isConnected(self, x, y):
+        return self.findRep(x) == self.findRep(y)
+    
+    
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+    
+        paths = UnionFind(n, edges)
+        return paths.isConnected(source, destination)
         
                 
             
