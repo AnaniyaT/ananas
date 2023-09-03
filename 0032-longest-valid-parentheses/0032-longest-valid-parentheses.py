4d326fd70f1108977e41f6c10b563846bff60fe1
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-09-03 15:27:18 +0300

    Time: 52 ms (53.63%), Space: 17.1 MB (20.93%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,6 +1,7 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         valids = [0 for _ in range(len(s) + 1)]
+        ends = []
         
         stak = []
         for ind, p in enumerate(s):
@@ -10,11 +11,12 @@ class Solution:
                 if stak:
                     start = stak.pop()
                     valids[ind] = ind - start + 1
+                    ends.append(ind)
                 else:
                     prev = 0
                     
         maxx = 0
-        for ind in range(len(s)):
+        for ind in ends:
             valids[ind] += valids[ind - valids[ind]]
             maxx = max(maxx, valids[ind])
             
