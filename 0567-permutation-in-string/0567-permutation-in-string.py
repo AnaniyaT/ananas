9fa278be0a3e84e1093ab4c7cac4a6be42f76586
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-10-10 20:42:14 +0300

    Time: 62 ms (82.60%), Space: 16.2 MB, (92.33% )

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,26 +1,23 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        wSize = len(s1)
-        target = Counter(s1)
-        
-        current = Counter(s2[:wSize])
-        if current == target:
-            return 1
+        counts1 = Counter(s1)
         
         l = 0
-        
-        for r in range(wSize, len(s2)):
-            right = s2[r]
-            left = s2[l]
-            current[right] = current.get(right, 0) + 1
+        fulfilled = 0
+        cur = {}
+        for r in range(len(s2)):
+            cur[s2[r]] = cur.get(s2[r], 0) + 1
+            if cur[s2[r]] == counts1.get(s2[r], 0):
+                fulfilled += 1
             
-            current[left] -= 1
-            if not current[left]:
-                current.pop(left)
-            l += 1
-            
-            if current == target:
-                return 1
+            if r - l >= len(s1):
+                if cur[s2[l]] == counts1.get(s2[l], 0):
+                    fulfilled -= 1
+                    
+                cur[s2[l]] -= 1
+                l += 1
                 
-        return 0
-        
+            if fulfilled == len(counts1):
+                return True
+            
+        return False
