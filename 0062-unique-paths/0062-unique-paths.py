8545095be4a3c7779f5016af6fabcb1837b70c73
Anan <49951520+AnaniyaT@users.noreply.github.com>
2023-10-17 06:08:10 +0300

    Update 0062-unique-paths.py

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,11 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        grid = [[0 for _ in range(n+1)] for _ in range(m+1)]
-        grid[0][0] = 1
+        r = n - 1
+        d = m - 1
         
-        for row in range(m):
-            for col in range(n):
-                ways = grid[row-1][col] + grid[row][col-1]
-                grid[row][col] += ways
-                
-        return grid[m-1][n-1]
+        return factorial(r + d) // (factorial(r) * factorial(d))
